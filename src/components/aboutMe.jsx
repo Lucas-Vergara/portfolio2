e3df9b8a6bc7bfbfe9ed1fa40a7f7deb3bb8e5c3
Lucas-Vergara <lucasvergara1@gmail.com>
2021-03-03 20:52:11 -0300

    ortografia

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -18,7 +18,7 @@ const AboutMe = () => {
         <h1 className='title2'>Sobre Mi</h1>
         <p className='summary' sryle={{color: 'black'}}>
         Profesional interesado en optimizar tareas mediante la implementación de nuevas tecnologías y procesos.
-        Interezado en la creación de grupos eficientes de trabajo. Con mucho interés por seguir las últimas tendencias en tecnología, herramientas y desarrollo de aplicaciones. Habitualmente buscando cursos para especializarse. He completado cursos dictados por google, y los curriculums de React, NodeJs y RoR en <a href="https://www.theodinproject.com" target='_blanck'>"The Odin Project"</a>
+        Interesado en la creación de grupos eficientes de trabajo. Preocupado de seguir las últimas tendencias en tecnología, herramientas y desarrollo de aplicaciones. Habitualmente buscando cursos para especializarse. He completado cursos dictados por google, y los curriculums de React, NodeJs y RoR en <a href="https://www.theodinproject.com" target='_blanck'>"The Odin Project"</a>
         </p>
         <p className='summary'> 
           Puedes revisar mi C.V. <a href="https://cvonline.me/@whlrgei4v9" target='_blanck'>aquí</a>.
